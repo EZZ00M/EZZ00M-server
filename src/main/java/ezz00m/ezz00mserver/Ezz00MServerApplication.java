587dcc084d03e649a4f4ef7f4fd3b6a6812fd434
영서 <dudrhy12@sookmyung.ac.kr>
2024-11-28 00:27:12 +0900

    Chore: 프로젝트 초기 세팅

---
+++ src/main/java/ezz00m/ezz00mserver/Ezz00MServerApplication.java
@@ -0,0 +1,13 @@
+package ezz00m.ezz00mserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ezz00MServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ezz00MServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ezz00m/ezz00mserver/Ezz00MServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ezz00m.ezz00mserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ezz00MServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
