32c27b85d24d27215895ea7910c527d7941071a5
영서 <dudrhy12@sookmyung.ac.kr>
2025-01-08 02:13:46 +0900

    Chore: Application 수정

--- src/main/java/ezz00m/ezz00mserver/Ezz00MServerApplication.java
+++ src/main/java/ezz00m/ezz00mserver/Ezz00MServerApplication.java
@@ -2,8 +2,9 @@ package ezz00m.ezz00mserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class Ezz00MServerApplication {
 
     public static void main(String[] args) {
