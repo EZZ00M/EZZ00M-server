a95d65e4a09da44c57bf87473df4d985a1277542
영서 <dudrhy12@sookmyung.ac.kr>
2025-01-11 14:38:44 +0900

    Feat: Error 처리

--- src/main/java/ezz00m/ezz00mserver/global/codes/ErrorCode.java
+++ src/main/java/ezz00m/ezz00mserver/global/codes/ErrorCode.java
@@ -16,7 +16,8 @@ public enum ErrorCode {
 
     CSV_NOT_FOUND(HttpStatus.NOT_FOUND, "CSV 파일이 아닙니다."),
     FILE_READ_FAIL(HttpStatus.BAD_REQUEST, "파일 형식이 잘못 되었습니다.."),
-    ANALYZED_FAIL(HttpStatus.UNPROCESSABLE_ENTITY, "분석에 실패하였습니다.");
+    ANALYZED_FAIL(HttpStatus.UNPROCESSABLE_ENTITY, "분석에 실패하였습니다."),
+    SAVE_FILE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "파일 저장에 실패하였습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
